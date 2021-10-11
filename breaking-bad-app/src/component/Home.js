073ccd29aacede1bdc95bbf1982ca40909c464d4
Oğuz GÜL <ogzgl06@gmail.com>
2021-10-11 17:56:29 +0300

    List of the fetching data

--- breaking-bad-app/src/component/Home.js
+++ breaking-bad-app/src/component/Home.js
@@ -1,17 +1,45 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCharacters } from "../redux/charactersSlice";
+import Masonry from "react-masonry-css";
+import "./Home.css";
+import Loading from "./Loading";
+import Error from "./Error";
 
 function Home() {
-  const data = useSelector((state) => state.characters);
+  const characters = useSelector((state) => state.characters.items);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
   const dispatch = useDispatch();
-  console.log(data);
 
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
-  return <div>Home</div>;
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error message={error} />;
+  }
+
+  return (
+    <div>
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {characters.map((data) => (
+          <div key={data.char_id}>
+            <img alt={data.name} src={data.img} className="character" />
+            <div className="char_name">{data.name}</div>
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default Home;
--- breaking-bad-app/src/redux/charactersSlice.js
+++ breaking-bad-app/src/redux/charactersSlice.js
@@ -4,7 +4,9 @@ import axios from "axios";
 export const fetchCharacters = createAsyncThunk(
   "characters/getCharacters",
   async () => {
-    const resp = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
+    const resp = await axios(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=12`
+    );
     return resp.data;
   }
 );
@@ -13,11 +15,20 @@ export const charactersSlice = createSlice({
   name: "characters",
   initialState: {
     items: [],
+    isLoading: false,
   },
   reducers: {},
   extraReducers: {
+    [fetchCharacters.pending]: (state, action) => {
+      state.isLoading = true;
+    },
     [fetchCharacters.fulfilled]: (state, action) => {
-      console.log(action.payload);
+      state.items = action.payload;
+      state.isLoading = false;
+    },
+    [fetchCharacters.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
     },
   },
 });
