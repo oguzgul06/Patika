41aa47d7dc009e6845718c122f9e890f6273fa09
Oğuz GÜL <ogzgl06@gmail.com>
2021-10-12 15:23:08 +0300

    Initialize project and createEntityAdaptor

---
+++ contacts-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Contacts from "./component/Contacts";
+
+function App() {
+  return (
+    <div className="app">
+      
+      <Contacts />
+    </div>
+  );
+}
+
+export default App;
---
+++ contacts-app/src/component/Form.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addContact, addContacts } from "../redux/contactSlice";
+import { nanoid } from "@reduxjs/toolkit";
+
+function Form() {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) return false;
+
+    const names = name.split(",");
+
+    dispatch(addContacts(names.map((name) => ({ id: nanoid(), name }))));
+    setName("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
